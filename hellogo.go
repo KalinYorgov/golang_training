eea2247e7d0f9b9059eff516e036395744f400f7
Kalin Yorgov <kalinyorgov@gmail.com>
2023-11-14 00:04:54 +0200

    Didn't had a lot of time today but still trying to keep it consistent! time: 1:43:11 / 3:49:14

--- hellogo.go
+++ hellogo.go
@@ -302,11 +302,72 @@ func main() {
 	// changeVal2(&f4)
 	// pl("f4 after func", f4)
 
-	pArr := [4]int{1, 2, 3, 4}
-	dblArrVals(&pArr)
-	pl(pArr)
+	// pArr := [4]int{1, 2, 3, 4}
+	// dblArrVals(&pArr)
+	// pl(pArr)
 
-	iSlice := []float64{11, 13, 17}
-	fmt.Printf("Average : %.3f\n", getAverage(iSlice...))
+	// iSlice := []float64{11, 13, 17}
+	// fmt.Printf("Average : %.3f\n", getAverage(iSlice...))
+
+	// f, err := os.Create("data.txt")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer f.Close()
+	// iPrimeArr := []int{2, 3, 5, 7, 11}
+	// var sPrimeArr []string
+	// for _, i := range iPrimeArr {
+	// 	sPrimeArr = append(sPrimeArr, strconv.Itoa(i))
+	// }
+	// for _, num := range sPrimeArr {
+	// 	_, err := f.WriteString(num + "\n")
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// }
+	// f, err = os.Open("data.txt")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer f.Close()
+
+	// scan1 := bufio.NewScanner(f)
+	// for scan1.Scan() {
+	// 	pl("Prime :", scan1.Text())
+	// }
+	// if err := scan1.Err(); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	/*
+		Exactly one of O_RDONLY, O_WRONLY, or O_RDWR must be specified.
+		O_RDONLY : open the file read-only.
+		O_WRONLY : open the file write-only.
+		O_RDWR : open the file read-write.
+
+		These can be or'ed with the following:
+
+		O_APPEND : append data to the file when writing.
+		O_CREATE : create a new file if none exists.
+		O_EXCL : used with O_CREATE, file must not exist.
+		O_SYNC : open for synchronous I/O.
+
+	*/
+
+	// _, err := os.Stat("data.txt")
+	// if errors.Is(err, os.ErrNotExist) {
+	// 	pl("File does not exist")
+	// } else {
+	// 	pl("File exists")
+	// 	f, err := os.OpenFile("data.txt",
+	// 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	defer f.Close()
+	// 	if _, err := f.WriteString("13\n"); err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// }
 
 }
